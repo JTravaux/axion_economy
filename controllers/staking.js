142124e666e9e57c381e671a658ccbdf4b600192
Jordan <jordan.travaux@hotmail.com>
2020-11-18 14:28:33 -0500

    fix

--- controllers/staking.js
+++ controllers/staking.js
@@ -6,6 +6,112 @@ const CONTRACT_FIRST_BLOCK = 11236016;
 let currentBlock;
 let db_client;
 
+const calculateEcosystem = data => {
+    const results = {
+        totals: {
+            stakers: 0,
+            staked_axn: 0,
+            last_updated: 0,
+        },
+        shrimp: {
+            count: 0,
+            totalAxn: 0
+        },
+        crab: {
+            count: 0,
+            totalAxn: 0
+        },
+        fish: {
+            count: 0,
+            totalAxn: 0
+        },
+        octopus: {
+            count: 0,
+            totalAxn: 0
+        },
+        dolphin: {
+            count: 0,
+            totalAxn: 0
+        },
+        tigerShark: {
+            count: 0,
+            totalAxn: 0
+        },
+        greatWhite: {
+            count: 0,
+            totalAxn: 0
+        },
+        whale: {
+            count: 0,
+            totalAxn: 0
+        },
+    }
+
+    data.forEach(r => {
+        if (r.balance >= 1 && r.balance <= 999) {
+            results.totals.stakers++;
+            results.shrimp.count++;
+            results.totals.staked_axn += r.balance;
+            results.shrimp.totalAxn += r.balance;
+        }
+        else if (r.balance >= 1000 && r.balance <= 999999) {
+            results.totals.stakers++;
+            results.crab.count++;
+            results.totals.staked_axn += r.balance;
+            results.crab.totalAxn += r.balance;
+        }
+        else if (r.balance >= 1000000 && r.balance <= 9999999) {
+            results.totals.stakers++;
+            results.fish.count++;
+            results.totals.staked_axn += r.balance;
+            results.fish.totalAxn += r.balance;
+        }
+        else if (r.balance >= 10000000 && r.balance <= 49999999) {
+            results.totals.stakers++;
+            results.octopus.count++;
+            results.totals.staked_axn += r.balance;
+            results.octopus.totalAxn += r.balance;
+        }
+        else if (r.balance >= 50000000 && r.balance <= 99999999) {
+            results.totals.stakers++;
+            results.dolphin.count++;
+            results.totals.staked_axn += r.balance;
+            results.dolphin.totalAxn += r.balance;
+        }
+        else if (r.balance >= 100000000 && r.balance <= 499999999) {
+            results.totals.stakers++;
+            results.tigerShark.count++;
+            results.totals.staked_axn += r.balance;
+            results.tigerShark.totalAxn += r.balance;
+        }
+        else if (r.balance >= 500000000 && r.balance <= 999999999) {
+            results.totals.stakers++;
+            results.greatWhite.count++;
+            results.totals.staked_axn += r.balance;
+            results.greatWhite.totalAxn += r.balance;
+        }
+        else if (r.balance >= 1000000000) {
+            results.totals.stakers++;
+            results.whale.count++;
+            results.totals.staked_axn += r.balance;
+            results.whale.totalAxn += r.balance;
+        }
+    })
+
+    results.shrimp.totalAxn = Math.floor(results.shrimp.totalAxn);
+    results.crab.totalAxn = Math.floor(results.crab.totalAxn);
+    results.fish.totalAxn = Math.floor(results.fish.totalAxn);
+    results.octopus.totalAxn = Math.floor(results.octopus.totalAxn);
+    results.dolphin.totalAxn = Math.floor(results.dolphin.totalAxn);
+    results.tigerShark.totalAxn = Math.floor(results.tigerShark.totalAxn);
+    results.greatWhite.totalAxn = Math.floor(results.greatWhite.totalAxn);
+    results.whale.totalAxn = Math.floor(results.whale.totalAxn);
+
+    results.totals.last_updated = Date.now();
+
+    return results;
+}
+
 const _getSavedData = () => {
     return new Promise(async (resolve, reject) => {
         if (db_client) {
@@ -142,7 +248,7 @@ const _fetchAllStakeEvents = () => {
             }
 
             totalsCache = result;
-            _saveToDB(result);
+            //_saveToDB(result);
             resolve(result);
         }).catch(err => { reject(err) })
     })
